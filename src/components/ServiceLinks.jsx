1be6de3afe74f3a230740708281a59e634e890b3
DBrush <coldnebraska@gmail.com>
2024-05-15 18:22:05 -0500

    added service navigation images to homepage

---
+++ src/components/ServiceLinks.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+
+import rangeRover from '../assets/images/range_rover.jpg'
+import porsche from '../assets/images/porsche_gt3.jpg'
+import lamboDuo from '../assets/images/lamborghini_duo.jpg'
+import lamboSingle from '../assets/images/lamborghini.jpg'
+import bmw from '../assets/images/bmw.jpg'
+import blackPorsche from '../assets/images/porsche_gts.jpg'
+
+export default function ServiceLinks() {
+  const [active, setActive] = useState('')
+
+  const top = [
+    {
+      id: 'window-tint',
+      title: 'WINDOW TINTING',
+      text: 'PROTECTION MEETS STYLE',
+      img: rangeRover
+    },
+    {
+      id: 'paint-protection',
+      title: 'PAINT PROTECTION',
+      text: 'SHIELD AGAINST DAMAGE',
+      img: porsche
+    },
+    {
+      id: 'ceramic-coating',
+      title: 'CERAMIC COATING',
+      text: 'RESTORE AND REVITALIZE',
+      img: lamboDuo
+    }
+  ]
+
+  const bottom = [
+    {
+      id: 'detailing',
+      title: 'DETAILING',
+      text: "REVIVE YOUR VEHICLE'S SHINE",
+      img: lamboSingle
+    },
+    {
+      id: 'xpel',
+      title: 'XPEL',
+      text: 'ADVANCED VEHICLE ARMOR',
+      img: bmw
+    },
+    {
+      id: 'car-wash',
+      title: 'CAR WASH',
+      text: 'CLEANLINESS REIMAGINED',
+      img: blackPorsche
+    }
+  ]
+
+  return (
+    <>
+      <div className="top">
+        {top.map((service) => {
+          return (
+            <div key={1} className='top-img' style={active == service.id ? {width: '50%'} : {width: '33.3%'}} onMouseEnter={() => setActive(service.id)}>
+              <img src={service.img} />
+              <div className="text-overlay">
+                <h1>{service.title}</h1>
+                <p>{service.text}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      <div className="bottom">
+        {bottom.map((service) => {
+          return (
+            <div key={1} className='bottom-img' style={active == service.id ? {width: '50%'} : {width: '33.3%'}} onMouseEnter={() => setActive(service.id)}>
+              <img src={service.img} />
+              <div className="text-overlay">
+                <h1>{service.title}</h1>
+                <p>{service.text}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
--- src/pages/apex_pages/HomePage.jsx
+++ src/pages/apex_pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Services from '../../components/Services'
+import ServiceLinks from '../../components/ServiceLinks'
 
 import aboutImg from '../../assets/images/homepage_about.png'
 import aboutLogo from '../../assets/images/apex_orange_logo.png'
@@ -6,21 +7,25 @@ import aboutLogo from '../../assets/images/apex_orange_logo.png'
 export default function HomePage() {
   return (
     <div>
-        <div className='services-nav'>
-          <Services />
+      <div className='services-display'>
+        <Services />
+      </div>
+
+      <div className="about">
+        <div className='about-text'>
+          <h1 id='shadow'>About</h1>
+          <h1>PROTECTING YOUR</h1>
+          <h1 id='highlight'>INVESTMENT</h1>
+          <p>Welcome to Apex Auto Shield, where meticulous attention to detail meets cutting-edge technology in automotive care. Based in Leander, TX, we offer a comprehensive range of services, including window tinting, paint protection film, paint correction, ceramic coating, and detailing. With extensive expertise in the field, we are committed to enhancing the appearance and extending the life of your vehicle, using only the highest-quality products in the industry. Our customer-centric approach ensures an exceptional service experience, tailored to your unique needs. Join the Apex Auto Shield family and let us take your vehicle to the apex of aesthetic appeal and protection.</p>
         </div>
-        <div className="about">
-          <div className='about-text'>
-            <h1 id='shadow'>About</h1>
-            <h1>PROTECTING YOUR</h1>
-            <h1 id='highlight'>INVESTMENT</h1>
-            <p>Welcome to Apex Auto Shield, where meticulous attention to detail meets cutting-edge technology in automotive care. Based in Leander, TX, we offer a comprehensive range of services, including window tinting, paint protection film, paint correction, ceramic coating, and detailing. With extensive expertise in the field, we are committed to enhancing the appearance and extending the life of your vehicle, using only the highest-quality products in the industry. Our customer-centric approach ensures an exceptional service experience, tailored to your unique needs. Join the Apex Auto Shield family and let us take your vehicle to the apex of aesthetic appeal and protection.</p>
-          </div>
-          <div className='homepage-about-img'>
-            <img src={aboutImg} id='about-logo' />
-            <img src={aboutLogo} id='logo-overlay' />
-          </div>
+
+        <div className='homepage-about-img'>
+          <img src={aboutImg} id='about-logo' />
+          <img src={aboutLogo} id='logo-overlay' />
         </div>
+      </div>
+
+      <ServiceLinks />
     </div>
   )
 }
