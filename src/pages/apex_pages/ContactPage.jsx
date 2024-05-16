ad55837c582141eab9452fee34e28048c323d320
DBrush <coldnebraska@gmail.com>
2024-05-16 15:14:33 -0500

    finished apex contact page and styling

--- src/pages/apex_pages/ContactPage.jsx
+++ src/pages/apex_pages/ContactPage.jsx
@@ -1,53 +1,100 @@
-import Map from '../../components/Map'
+import cadillac from '../../assets/contactImages/cadillac.png'
+import { Icon } from "@iconify/react"
 
 export default function ContactPage() {
   return (
-    <div className="page-layout-1">
-      <div className="top">
-        <div className="info-bar">
-          <div className="info">
-            <h4>Phone:</h4>
-            <p>(512)346-2992</p>
-          </div>
+    <div className="contact-page">
+      <h1 id='shadow-main'>C O N T A C T</h1>
+      <div className="side-by-side">
+        <img src={cadillac} id='z-index' />
 
-          <div className="bar"></div>
+        <div>
+          <h1 id='highlight'>GET IN TOUCH</h1>
+          <p>We look forward to protecting and preserving your ride, for a service that will last. Get in touch with one of our service pros today to schedule your detail or to have any questions answered.</p>
+          
+          <div className='icons'>
+            <a href='https://www.facebook.com' target='_blank'>
+              <Icon icon='bxl:facebook' id='icon' />
+            </a>
 
-          <div className="info">
-            <h4>Address:</h4>
-            <p>6102 Spicewood Springs Road, Austin, TX 78759</p>
-          </div>
-
-          <div className="bar middle-bar"></div>
-
-          <div className="info">
-            <h4>Email:</h4>
-            <p>scschool@flash.net</p>
-          </div>
-
-          <div className="bar"></div>
-
-          <div className="info">
-            <h4>Hours:</h4>
-            <p>7:30am to 5:30pm</p>
+            <a href='https://instagram.com' target='_blank'>
+              <Icon icon='bxl:instagram' id='icon' />
+            </a>
           </div>
         </div>
       </div>
 
-      <div className="contact-bottom">
-        <Map />
-
-        <div className="contact-form">
-          <h2>Schedule a visit or ask a question!</h2>
-          <div className="contact-info">
-            <input placeholder="Name"></input>
-            <input placeholder="Email"></input>
-          </div>
-          <div className="msg-details">
-            <input placeholder="Subject"></input>
-            <textarea placeholder="Message"></textarea>
-          </div>
-          <button>Send</button>
+      <div className='info'>
+        <div>
+          <h1 id='highlight'>Phone</h1>
+          <p>(512)585-9842</p>
         </div>
+
+        <div>
+          <h1 id='highlight'>Address</h1>
+          <p>607 Leander Dr. Suite 300 Leander, TX 78641</p>
+        </div>
+
+        <div>
+          <h1 id='highlight'>Email</h1>
+          <p>info@apexautoshieldtx.com</p>
+        </div>
+      </div>
+
+      <div className="contact-form">
+        <div className="contact-row">
+          <p>Name</p>
+          <div className='name'>
+            <input placeholder='First'></input>
+            <input placeholder='Last'></input>
+          </div>
+        </div>
+
+        <div className="phone">
+          <p>Phone</p>
+          <input placeholder='(123) 456-7890'></input>
+        </div>
+
+        <div className="email">
+          <p>Email</p>
+          <input></input>
+        </div>
+
+        <div className='contact-row'>
+          <div className="vehicle">
+            <div>
+              <p>Vehicle Year</p>
+              <input></input>
+            </div>
+
+            <div>
+              <p>Vehicle Make</p>
+              <input></input>
+            </div>
+
+            <div>
+              <p>Vehicle Model</p>
+              <input></input>
+            </div>
+          </div>
+        </div>
+
+        <div className="contact-row">
+          <p>Select Your Service</p>
+          <select>
+            <option>Window Tint</option>
+            <option>Paint Protection Film</option>
+            <option>Ceramic Coating</option>
+            <option>Detailing</option>
+          </select>
+        </div>
+
+        <div className="contact-row">
+          <p>Message</p>
+          <textarea></textarea>
+        </div>
+
+        <button>Submit</button>
       </div>
     </div>
   )
