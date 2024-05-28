53c142dec85584fa5fb6dd83535364173a73a8f4
DBrush <coldnebraska@gmail.com>
2024-05-28 15:43:11 -0500

    added links to service navigation images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,28 +11,39 @@ export default function Services() {
   return (
     <div className='services-display'>
       <div key={1} className="service-img" style={active == 'ppf' ? {width: '40%'} : {width: '20%'}} onMouseEnter={() => setActive('ppf')}>
-        <div className="blur" style={active == 'ppf' ? {filter: 'blur(0px)'} : {}}>
-          <img src={ppf} />
-        </div>
-        <h1 className='rotate'>Paint Protection</h1>
+        <a href='/apex/services'>
+          <div className="blur" style={active == 'ppf' ? {filter: 'blur(0px)'} : {}}>
+            <img src={ppf} />
+          </div>
+          <h1 className='rotate'>Paint Protection</h1>
+        </a>
       </div>
+
       <div key={2} className="service-img" style={active == 'detailing' ? {width: '40%'} : {width: '20%'}} onMouseEnter={() => setActive('detailing')}>
-        <div className="blur" style={active == 'detailing' ? {filter: 'blur(0px)'} : {}}>
-          <img src={detailing} />
-        </div>
-        <h1 className='rotate'>Detailing</h1>
+        <a href='/apex/services'>
+          <div className="blur" style={active == 'detailing' ? {filter: 'blur(0px)'} : {}}>
+            <img src={detailing} />
+          </div>
+          <h1 className='rotate'>Detailing</h1>
+        </a>
       </div>
+
       <div key={3} className="service-img" style={active == 'correction' ? {width: '40%'} : {width: '20%'}} onMouseEnter={() => setActive('correction')}>
-        <div className="blur" style={active == 'correction' ? {filter: 'blur(0px)'} : {}}>
-          <img src={correction} />
-        </div>
-        <h1 className='rotate'>Correction</h1>
+        <a href='/apex/services'>
+          <div className="blur" style={active == 'correction' ? {filter: 'blur(0px)'} : {}}>
+            <img src={correction} />
+          </div>
+          <h1 className='rotate'>Correction</h1>
+        </a>
       </div>
+
       <div key={4} className="service-img" style={active == 'tint' ? {width: '40%'} : {width: '20%'}} onMouseEnter={() => setActive('tint')}>
-        <div className="blur" style={active == 'tint' ? {filter: 'blur(0px)'} : {}}>
-          <img src={tint} />
-        </div>
-        <h1 className='rotate'>Window Tint</h1>
+        <a href='/apex/services'>
+          <div className="blur" style={active == 'tint' ? {filter: 'blur(0px)'} : {}}>
+            <img src={tint} />
+          </div>
+          <h1 className='rotate'>Window Tint</h1>
+        </a>
       </div>
     </div>
   )
